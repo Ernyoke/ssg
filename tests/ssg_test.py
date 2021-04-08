2ba62ebd095f4b98ba9ad4cfb50f79dd030f32f4
Ernyoke <ervin_szilagyi@outlook.com>
2021-04-08 17:55:42 +0300

    Remove outdated test.

--- tests/ssg_test.py
+++ tests/ssg_test.py
@@ -8,10 +8,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(ssg.replace_md_with_html('<a href="asd.md"></a><a href="x.html"></a>'),
                          '<a href="asd.html"></a><a href="x.html"></a>')
 
-    # def test_add_base_path(self):
-    #     self.assertEqual(ssg.add_base_path('<head></head>', 'base.html'),
-    #                      '<head>\n <base href="base.html"/>\n</head>')
-
 
 if __name__ == '__main__':
     unittest.main()
